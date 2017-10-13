07cc5d70521db6cb4e30c343585427a674e7b3c9
eouia <eouia0819@gmail.com>
2017-10-13 14:39:00 +0200

    first release

---
+++ MMM-Remote-Control-Repository.js
@@ -0,0 +1,24 @@
+/* Magic Mirror
+ * Module: repository updater plugin for MMM-Remote-Control
+ *
+ * By eouia
+ * MIT Licensed.
+ */
+
+Module.register("MMM-Remote-Control-Repository", {
+	start: function() {
+		//do nothing
+	},
+
+	notificationReceived: function(noti, payload, sender) {
+		if (noti == "DOM_OBJECTS_CREATED") {
+			var self = this
+			var modules = MM.getModules().enumerate(function(m){
+				if (m.name == "MMM-Remote-Control") {
+					self.sendSocketNotification("START")
+				}
+			})
+		}
+	}
+
+})
---
+++ node_helper.js
@@ -0,0 +1,62 @@
+/* Magic Mirror
+ * Module: repository updater plugin for MMM-Remote-Control
+ *
+ * By eouia
+ * MIT Licensed.
+ */
+
+const NodeHelper = require("node_helper")
+const path = require("path")
+const fs = require("fs")
+const https = require("https");
+const repository = "https://github.com/MichMich/MagicMirror/wiki/MagicMirror%C2%B2-Modules"
+const moduleJson = "modules.json"
+
+module.exports = NodeHelper.create({
+	start: function() {
+		//do nothing;
+	},
+
+	socketNotificationReceived: function(message, payload) {
+		if (message == "START") {
+			this.readRepository()
+		}
+	},
+
+	readRepository: function() {
+		console.log("[RCREPO] Repository scanning...")
+		var modules = [];
+		var body = "";
+		https.get(repository, function(res) {
+			res.setEncoding("utf8");
+			res.on("data", function(chunk) {
+				body += chunk;
+			});
+			res.on("end", function() {
+				var pattern = /<tr>\s<td.*><a href="(.*)">(.*)<\/a><\/td>\s<td.*>(.*)<\/td>\s<td[^>]*>(.*)<\/td>\s<\/tr>/gm;
+				var result;
+				while ((result = pattern.exec(body)) !== null) {
+					var module = {
+						url: result[1],
+						longname: result[2],
+						author: result[3],
+						desc: result[4],
+						id: result[3] + "/" + result[2],
+					}
+					modules.push(module);
+				}
+				var json = JSON.stringify(modules);
+				var jsonPath = path.resolve(__dirname, "../MMM-Remote-Control", moduleJson)
+				fs.writeFile(jsonPath, json, "utf8", function(err, data) {
+					if (err) {
+						console.log("[RCREPO] modules.json updating fail:" + err.message)
+					} else {
+						console.log("[RCREPO] modules.json is updated.")
+					}
+				})
+			})
+		}).on("error", function(e) {
+			console.log("[RCREPO] Repository reading error: " + e.message);
+		});
+	}
+})
