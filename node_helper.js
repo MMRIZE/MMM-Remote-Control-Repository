d911b55f2de90c4732c8f8a7f4661e78eaa9c67a
eouia <eouia0819@gmail.com>
2017-10-13 15:00:29 +0200

    lint

--- node_helper.js
+++ node_helper.js
@@ -8,7 +8,7 @@
 const NodeHelper = require("node_helper")
 const path = require("path")
 const fs = require("fs")
-const https = require("https");
+const https = require("https")
 const repository = "https://github.com/MichMich/MagicMirror/wiki/MagicMirror%C2%B2-Modules"
 const moduleJson = "modules.json"
 
@@ -25,16 +25,16 @@ module.exports = NodeHelper.create({
 
 	readRepository: function() {
 		console.log("[RCREPO] Repository scanning...")
-		var modules = [];
-		var body = "";
+		var modules = []
+		var body = ""
 		https.get(repository, function(res) {
-			res.setEncoding("utf8");
+			res.setEncoding("utf8")
 			res.on("data", function(chunk) {
-				body += chunk;
-			});
+				body += chunk
+			})
 			res.on("end", function() {
-				var pattern = /<tr>\s<td.*><a href="(.*)">(.*)<\/a><\/td>\s<td.*>(.*)<\/td>\s<td[^>]*>(.*)<\/td>\s<\/tr>/gm;
-				var result;
+				var pattern = /<tr>\s<td.*><a href="(.*)">(.*)<\/a><\/td>\s<td.*>(.*)<\/td>\s<td[^>]*>(.*)<\/td>\s<\/tr>/gm
+				var result
 				while ((result = pattern.exec(body)) !== null) {
 					var module = {
 						url: result[1],
@@ -43,9 +43,9 @@ module.exports = NodeHelper.create({
 						desc: result[4],
 						id: result[3] + "/" + result[2],
 					}
-					modules.push(module);
+					modules.push(module)
 				}
-				var json = JSON.stringify(modules);
+				var json = JSON.stringify(modules)
 				var jsonPath = path.resolve(__dirname, "../MMM-Remote-Control", moduleJson)
 				fs.writeFile(jsonPath, json, "utf8", function(err, data) {
 					if (err) {
@@ -56,7 +56,7 @@ module.exports = NodeHelper.create({
 				})
 			})
 		}).on("error", function(e) {
-			console.log("[RCREPO] Repository reading error: " + e.message);
-		});
+			console.log("[RCREPO] Repository reading error: " + e.message)
+		})
 	}
 })
